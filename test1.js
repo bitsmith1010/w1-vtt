4274c42b72d12feba7ac0d6a2fc2a7938c80bcdb
dani <dan.yaniv@bitmovin.com>
2021-05-05 11:49:16 -0400

    test for vtt style

--- test1.js
+++ test1.js
@@ -3,11 +3,15 @@ const config = {
   logs: {
       level: "debug"
   },
+  "playback": {
+//    "preferredTech": [{"player":"html5","streaming":"hls"}]
+  },
   network: {
     preprocessHttpResponse: function(type, response) {
       if (type == "media/subtitles") {
         let data = new DataView(response.body);
         let decoder = new TextDecoder("utf-8");
+        console.log("***** ON SUBTITLE REQUEST ****");
         let vttProcessed = decoder
           .decode(data)
           .split(/\n([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}[^\n]*)\n/)
@@ -16,7 +20,6 @@ const config = {
               el.replaceAll( /(.)\n(.)/g, "$1<br/>$2" )
           )
           .join("\n");
-
         let textEncoder = new TextEncoder("utf-8");
         let dataProcessed = textEncoder.encode(vttProcessed);
         response.body = dataProcessed.buffer;
@@ -41,7 +44,7 @@ const container = document.getElementById('player-test1');
 const player = new bitmovin.player.Player(container, config);
 
 const source = {
-    "hls": "https://bitmovin-amer-public.s3.amazonaws.com/internal/dani/Wed_Apr_21_17%3A48%3A00_EDT_2021/zd7929-test1.m3u8",
+    "hls": "https://bitmovin-amer-public.s3.amazonaws.com/internal/dani/Wed_Apr_21_17%3A48%3A00_EDT_2021/w1-test1.m3u8"
   };
 player.load(source).then(
   function()
